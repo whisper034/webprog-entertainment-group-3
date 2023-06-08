3563aa6585bd0f40de27bbad5f3a1f4077c4799b
Rico0810 <ricotokan155@gmail.com>
2023-06-08 11:33:08 +0700

    #2-seeder-track-and-artist-buat seeder track dan benerin seeder artist dikit

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -54,8 +54,8 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'July 9, 2002',
             'photo_url' => '/images/artist/original/keena.png',
             'photo_url_hovered' => '/images/artist/hovered/keena.png',
-            'biodata' => "Behind her adorable visuals, this member of FIFTY FIFTY actually holds the position of the main rapper! Additionally, she also takes on the role of sub-vocalist within the group. Daebak!
-            Prior to debuting with FIFTY FIFTY, Keena was involved in creating an OST for the drama titled The Running Mates: Human Rights. She sang a song called Take Back My Life.",
+            'biodata' => "Behind her adorable visuals, this member of FIFTY FIFTY actually holds the position of the main rapper! Additionally, she also takes on the role of sub-vocalist within the group. Daebak!\n
+            Prior to debuting with FIFTY FIFTY, Keena was involved in creating an OST for the drama titled \"The Running Mates: Human Rights.\" She sang a song called \"Take Back My Life.\"",
             'created_at' => $nowDate
         ]);
 
@@ -68,8 +68,8 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'May 15, 2006',
             'photo_url' => '/images/artist/original/haerin.png',
             'photo_url_hovered' => '/images/artist/hovered/haerin.png',
-            'biodata' => "An interesting fact about Haerin is that her favorite foods are Korean cuisine and raw fish. Many people say that she resembles Tomie.
-            Haerin also greatly enjoys the scent of flowers. She used to play pansori, a traditional Korean genre of musical storytelling. Her nickname during school was Voice Fairy.",
+            'biodata' => "An interesting fact about Haerin is that her favorite foods are Korean cuisine and raw fish. Many people say that she resembles Tomie.\n
+            Haerin also greatly enjoys the scent of flowers. She used to play pansori, a traditional Korean genre of musical storytelling. Her nickname during school was \"Voice Fairy.\"",
             'created_at' => $nowDate
         ]);
 
@@ -79,7 +79,7 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'April 11, 2005',
             'photo_url' => '/images/artist/original/danielle.png',
             'photo_url_hovered' => '/images/artist/hovered/danielle.png',
-            'biodata' => "Danielle has an interesting fact: she likes the spring and fall seasons. Her favorite foods are her mother's cooking, vegetables, and fruits.
+            'biodata' => "Danielle has an interesting fact: she likes the spring and fall seasons. Her favorite foods are her mother's cooking, vegetables, and fruits.\n
             What's unique about Danielle is her ability to make various shapes with her tongue and her habit of wearing socks before going to bed.",
             'created_at' => $nowDate
         ]);
@@ -90,7 +90,7 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'October 6, 2004',
             'photo_url' => '/images/artist/original/hanni.png',
             'photo_url_hovered' => '/images/artist/hovered/hanni.png',
-            'biodata' => "An interesting fact about Hanni is that she likes all seasons except for summer. Her favorite foods are bread and meat. She is also skilled at playing the ukulele and is a big fan of One Direction.
+            'biodata' => "An interesting fact about Hanni is that she likes all seasons except for summer. Her favorite foods are bread and meat. She is also skilled at playing the ukulele and is a big fan of One Direction.\n
             Additionally, a notable trait of Hanni is her ability to sleep anywhere. She also has a unique hobby of photographing clouds.",
             'created_at' => $nowDate
         ]);
@@ -101,7 +101,7 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'April 21, 2008',
             'photo_url' => '/images/artist/original/hyein.png',
             'photo_url_hovered' => '/images/artist/hovered/hyein.png',
-            'biodata' => "Hyein, the maknae, has a strong liking for strawberries. She is currently learning English as her second language.
+            'biodata' => "Hyein, the maknae, has a strong liking for strawberries. She is currently learning English as her second language.\n
             An interesting fact about Hyein is her skill in taking photos of the other members. She also started her career as a child model at the age of 8.",
             'created_at' => $nowDate
         ]);
@@ -112,7 +112,7 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'May 7, 2004',
             'photo_url' => '/images/artist/original/minji.png',
             'photo_url_hovered' => '/images/artist/hovered/minji.png',
-            'biodata' => "It turns out that Minji has the ESTJ personality type according to the Myers-Briggs Type Indicator (MBTI). A brief explanation of the ESTJ type is that they are practical, realistic, fact-oriented, and decisive.
+            'biodata' => "It turns out that Minji has the ESTJ personality type according to the Myers-Briggs Type Indicator (MBTI). A brief explanation of the ESTJ type is that they are practical, realistic, fact-oriented, and decisive.\n
             Minji attended Hanlim Arts School. She also enjoys both the summer and winter seasons. Her favorite food is her mother's cooking and ice cream.",
             'created_at' => $nowDate
         ]);
@@ -125,7 +125,7 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'May 26, 2000',
             'photo_url' => '/images/artist/original/yeji.png',
             'photo_url_hovered' => '/images/artist/hovered/yeji.png',
-            'biodata' => "Yeji is the official leader of ITZY. She is also the oldest member of the group. In 2018, prior to to her debut with Itzy, she participated in SBS’ show The Fan but was eliminated in episode 5. Her motto is believe in yourself. Her English name is Lucy Hwang.",
+            'biodata' => "Yeji is the official leader of ITZY. She is also the oldest member of the group. In 2018, prior to to her debut with Itzy, she participated in SBS’ show The Fan but was eliminated in episode 5. Her motto is \"believe in yourself\". Her English name is Lucy Hwang.",
             'created_at' => $nowDate
         ]);
 
@@ -177,7 +177,7 @@ class ArtistSeeder extends Seeder
             'date_of_birth' => 'April 11, 2000',
             'photo_url' => '/images/artist/original/karina.png',
             'photo_url_hovered' => '/images/artist/hovered/karina.png',
-            'biodata' => "Karina is the official leader of aespa. She also holds the position of main dancer, lead rapper, and center. She is the tallest member in the group with a height of 167.8 cm (5'6''). Prior to her debut, Karina was a famous ulzzang (which means best face or good looking in Korean). She was a trainee for four years.",
+            'biodata' => "Karina is the official leader of aespa. She also holds the position of main dancer, lead rapper, and center. She is the tallest member in the group with a height of 167.8 cm (5'6). Prior to her debut, Karina was a famous ulzzang (which means best face or good looking in Korean). She was a trainee for four years.",
             'created_at' => $nowDate
         ]);
 
--- database/seeders/TrackSeeder.php
+++ database/seeders/TrackSeeder.php
@@ -18,7 +18,7 @@ class TrackSeeder extends Seeder
         // fifty-fifty --> album_id = 1
         Track::insert([
             'album_id' => 1,
-            'title' => 'FIFTY FIFTY (피프티피프티) - \'Higher\'',
+            'title' => 'FIFTY FIFTY (피프티피프티) - "Higher"',
             'composer' => 'Matt Thomson, Max Lynedoch Graham, George Tizzard, James Birt, Rick Parkhouse, Moa "Cazzi Opeia" Carlebecker (Sunshine), Ellen Berg (Sunshine)',
             'lyrics_by' => '강은유 (ARTiffect), Keena, Moa "Cazzi Opeia" Carlebecker (Sunshine), Ellen Berg (Sunshine)',
             'arranger' => 'ARCADES, Red Triangle, James Birt',
@@ -32,7 +32,7 @@ class TrackSeeder extends Seeder
 
         Track::insert([
             'album_id' => 1,
-            'title' => 'FIFTY FIFTY (피프티피프티) - \'Cupid\'',
+            'title' => 'FIFTY FIFTY (피프티피프티) - "Cupid"',
             'composer' => 'Adam von Mentzer, Mac Felländer-Tsai, Louise Udin',
             'lyrics_by' => 'SIAHN, AHIN, KEENA, Adam von Mentzer, Mac Felländer-Tsai, Louise Udin',
             'arranger' => 'SIAHN, Adam von Mentzer, Mac Felländer-Tsai, Louise Udin',
@@ -46,7 +46,7 @@ class TrackSeeder extends Seeder
 
         Track::insert([
             'album_id' => 1,
-            'title' => 'FIFTY FIFTY (피프티피프티) - \'Tell Me\'',
+            'title' => 'FIFTY FIFTY (피프티피프티) - "Tell Me"',
             'composer' => 'Julia Ross, Adam Kapit, Hautboi Rich',
             'lyrics_by' => 'SIAHN, 이앵두(153joombas), Julia Allyn Ross',
             'arranger' => 'Adam Kapit, Julia Ross',
@@ -60,7 +60,7 @@ class TrackSeeder extends Seeder
 
         Track::insert([
             'album_id' => 1,
-            'title' => 'FIFTY FIFTY (피프티피프티) - \'Lovin\' Me’',
+            'title' => 'FIFTY FIFTY (피프티피프티) - "Lovin Me"',
             'composer' => 'Albin Clern, Adam Von Mentzer, Lina Einarsson, Jonna Hall',
             'lyrics_by' => '김수민 (ARTiffect), Albin Clern, Adam Von Mentzer, Lina Einarsson, Jonna Hall',
             'arranger' => 'Albin Clern',
@@ -158,9 +158,161 @@ class TrackSeeder extends Seeder
         ]);
 
         // itzy --> album_id = 3
+        Track::insert([
+            'album_id' => 3,
+            'title' => 'ITZY (있지) "마.피.아. In the morning"',
+            'composer' => 'LYRE, J.Y. Park "The Asiansoul", earattack, KASS, Lee Hae-sol',
+            'lyrics_by' => 'J.Y. Park "The Asiansoul", KASS, danke, LYRE',
+            'arranger' => 'LYRE, J.Y. Park "The Asiansoul", Lee Hae-sol, earattack',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => 'Bang Jae-yeob',
+            'additional_data' => 'Music Video - Bang Jae-yeob',
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 3,
+            'title' => 'ITZY (있지) "Sorry Not Sorry"',
+            'composer' => 'Shim Eun-ji, Lee Min-young (EastWest), Yeul (1by1)',
+            'lyrics_by' => 'JQ, J14, TOMBOY (makeumine works)',
+            'arranger' => 'Lee Min-young (EastWest), Yeul (1by1)',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 3,
+            'title' => 'ITZY (있지) "KIDDING ME"',
+            'composer' => 'Kobee and Holy M',
+            'lyrics_by' => 'Kobee, Holy M, KASS',
+            'arranger' => 'Kobee, Holy M',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 3,
+            'title' => 'ITZY (있지) "WILD WILD WEST"',
+            'composer' => 'Andy Love (153/Joombas), earattack',
+            'lyrics_by' => 'earattack',
+            'arranger' => 'earattack',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 3,
+            'title' => 'ITZY (있지) "SHOOT!"',
+            'composer' => 'Alexander Pavelich, Maria Hazell, Jerker Olov Hansson, Gigi Grombacher, Joakim Harestad Haukaas, Evard Forre Erfjord',
+            'lyrics_by' => 'Lee Ha-jin, Mad Clown, Kim Seung-min',
+            'arranger' => 'Joki, Erfjord',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 3,
+            'title' => 'ITZY (있지) "TENNIS (0:0)"',
+            'composer' => 'Coach & Sendo, Cazzi Opeia, Alex Chang Jien',
+            'lyrics_by' => 'JQ, Vacation (makeumine works)',
+            'arranger' => 'Coach & Sendo',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
 
 
         // aespa --> album_id = 4
+        Track::insert([
+            'album_id' => 4,
+            'title' => 'AESPA (에스파) "Girls"',
+            'composer' => 'Ryan S. Jhun, Hanif Sabzevari (Hitmanic), Dennis "DeKo" Kordnejad (Hitmanic), Rodnae "Chikk" Bell, Pontus PJ Ljung, Yoo Young-jin',
+            'lyrics_by' => 'Yoo Young-jin',
+            'arranger' => 'Ryan S. Jhun, Hitmanic, Pontus "PJ" Ljung, Yoo Young-jin.',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 4,
+            'title' => 'AESPA (에스파) "도깨비불 (Illusion)"',
+            'composer' => 'G harah "PK" Degeddingseze, Patricia Battani',
+            'lyrics_by' => 'Lee Thor (Lalala Studio)',
+            'arranger' => 'G harah "PK" Degeddingseze, Patricia Battani, Steve Octave',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 4,
+            'title' => 'AESPA (에스파) "Lingo"',
+            'composer' => 'Alma Goodman, Gabriella Grombacher, Christina Galligan, Lisa "Lixa" Hickox',
+            'lyrics_by' => 'Song Jae-ri (Joombas)',
+            'arranger' => 'Lixa',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 4,
+            'title' => 'AESPA (에스파) "Life’s Too Short"',
+            'composer' => 'Becky Hill, Sam Klempner, Uzoechi Emenike',
+            'lyrics_by' => 'Jang Jeong-won',
+            'arranger' => 'Sam Klempner',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
+
+        Track::insert([
+            'album_id' => 4,
+            'title' => 'AESPA (에스파) "ICU (쉬어가도 돼)"',
+            'composer' => 'Ryan S. Jhun, Josh Cumbee, Nisha Asnani',
+            'lyrics_by' => 'Bae Hye-jin (Joombas)',
+            'arranger' => 'Ryan S. Jhun, Josh Cumbee',
+            'producer' => null,
+            'co_producer' => null,
+            'ar_director' => null,
+            'director' => null,
+            'additional_data' => null,
+            'created_at' => $nowDate
+        ]);
 
     }
 }
